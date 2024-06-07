11b60925bb3a69933bef1b980b355f15c81929f6
nnnnnnnao3 <165908897+nnnnnnnao3@users.noreply.github.com>
2024-06-08 00:18:15 +0900

    questionsコントローラーの記述

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,20 @@
 class QuestionsController < ApplicationController
+  
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
+  end
+
+  def create
+    Question.create(question_params)
+    redirect_to '/'
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content, :name)
   end
 end
