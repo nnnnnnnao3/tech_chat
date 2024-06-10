d3c829920fa84079101c833ae534f36b93044de0
nnnnnnnao3 <165908897+nnnnnnnao3@users.noreply.github.com>
2024-06-10 23:31:26 +0900

    ページ遷移と見た目調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'answers/create'
   root to: 'questions#index'
-  resources :questions, only: [:index, :new, :create]
+  resources :questions, only: [:index, :new, :create, :show] do
+    resources :answers, only: :create
+  end
 end
